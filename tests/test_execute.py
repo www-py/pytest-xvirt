0fe89419a018ef9fee103900d36033b48f80e4e0
Simone <simone.giacomelli@gmail.com>
2023-04-19 19:57:56 +0100

    checkpoint

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -1,33 +1,17 @@
 from pytest import Pytester
 
 
+# todo parte server
 def test_transport(pytester: Pytester) -> None:
     pytester.makeconftest(
         f"""
         def pytest_xvirt_notify(event,session):
+
             from xvirt.events import EvtCollectionFinish
             if isinstance(event, EvtCollectionFinish): 
                 stripped_ids = [x.split('::')[1] for x in event.node_ids]
                 print(f'HOOK: ' + ', '.join(stripped_ids))
-            # if isinstance(event, EvtRunReport
-    """
-    )
-    pytester.makepyfile(
-        """
-        import os
-        def test_a(): pass
-        def test_b(): pass
-        def test_c(): pass
-    """
-    )
-    res = pytester.runpytest()
-    res.stdout.fnmatch_lines_random(["*HOOK: test_a, test_b, test_c"])
 
-
-def test_transport_2(pytester: Pytester) -> None:
-    pytester.makeconftest(
-        """
-        def pytest_xvirt_notify(event, session):
             from xvirt.events import EvtRuntestLogreport
             if isinstance(event, EvtRuntestLogreport):
                 report = session.config.hook.pytest_report_from_serializable(config=session.config, data=event.data)
@@ -43,6 +27,8 @@ def test_transport_2(pytester: Pytester) -> None:
     """
     )
     res = pytester.runpytest()
+    res.stdout.fnmatch_lines_random(["*HOOK: test_a, test_b, test_c"])
+
     res.stdout.fnmatch_lines_random(["*HOOK: test_a"])
     res.stdout.fnmatch_lines_random(["*HOOK: test_b"])
     res.stdout.fnmatch_lines_random(["*HOOK: test_c"])
