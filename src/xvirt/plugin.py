af3773c1e0c7cd87c171fa78ee1881f5c4ff11c2
Simone <simone.giacomelli@gmail.com>
2023-04-29 17:02:08 +0100

    refactoring

--- src/xvirt/plugin.py
+++ src/xvirt/plugin.py
@@ -16,6 +16,29 @@ def pytest_addoption(parser):
     parser.addini('HELLO', 'Dummy pytest.ini setting')
 
 
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config) -> None:
+    config.pluginmanager.register(XvirtPlugin(config), "xvirt-plugin")
+
+
+class XvirtPlugin:
+
+    def __init__(self, config) -> None:
+        self._config = config
+
+    @pytest.hookimpl
+    def pytest_runtest_logreport(self, report):
+        session = report.session
+        config = session.config
+        del report.session
+        data = config.hook.pytest_report_to_serializable(config=config, report=report)
+        import json
+        data_json = json.dumps(data)
+        from .events import EvtRuntestLogreport
+        event = EvtRuntestLogreport(data)
+        session.config.hook.pytest_xvirt_notify(event=event, session=session)
+
+
 def pytest_pycollect_makemodule(module_path, path, parent):
     if parent.config.option.xvirt_package == '':
         return None
@@ -54,16 +77,3 @@ def pytest_runtest_makereport(item):
     out = yield
     report = out.get_result()
     report.session = item.session
-
-
-@pytest.hookimpl
-def pytest_runtest_logreport(report):
-    session = report.session
-    config = session.config
-    del report.session
-    data = config.hook.pytest_report_to_serializable(config=config, report=report)
-    import json
-    data_json = json.dumps(data)
-    from .events import EvtRuntestLogreport
-    event = EvtRuntestLogreport(data)
-    session.config.hook.pytest_xvirt_notify(event=event, session=session)
