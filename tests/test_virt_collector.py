85f4717867afd113a3953b48ad42cd3ccb8eb264
Simone <simone.giacomelli@gmail.com>
2023-05-03 20:19:02 +0100

    removed tree structure for Collectors/Item because the flat one is enough

--- tests/test_virt_collector.py
+++ tests/test_virt_collector.py
@@ -2,8 +2,6 @@ import json
 
 from pytest import Pytester
 
-from xvirt.collectors import _rebuild_tree, VCollector, VItem
-
 
 def test_collectors(pytester: Pytester):
     nodeids = [
@@ -31,48 +29,6 @@ def test_collectors(pytester: Pytester):
     for nodeid in nodeids:
         assert nodeid in stdout_lines
 
-
-def test_rebuild_tree__flat():
-    nodeids = ['mock_test.py::test_1', 'mock_test.py::test_2']
-
-    actual = _rebuild_tree(nodeids)
-    expect = {'mock_test.py': VCollector(
-        'mock_test.py', items=[
-            VItem('mock_test.py::test_1'),
-            VItem('mock_test.py::test_2')
-        ])
-    }
-    assert actual == expect
-
-
-def test_rebuild_tree__node_and_leaf():
-    nodeids = [
-        'foo/some_test.py::test_1',
-        'foo/some_test.py::test_2',
-        'foo/sub/sub_test.py::test_3',
-    ]
-
-    actual = _rebuild_tree(nodeids)
-    expect = {'foo': VCollector(
-        'foo', collectors={
-            'foo/some_test.py': VCollector(
-                'foo/some_test.py', items=[
-                    VItem('foo/some_test.py::test_1'),
-                    VItem('foo/some_test.py::test_2'),
-                ]
-            ),
-            'foo/sub': VCollector(
-                'foo/sub', collectors={
-                    'foo/sub/sub_test.py': VCollector(
-                        'foo/sub/sub_test.py', items=[
-                            VItem('foo/sub/sub_test.py::test_3'),
-                        ]
-                    )
-                }
-            )
-        }
-    )}
-    assert actual == expect
 
 
 def test_skip_module__should_skip_submodule2(pytester: Pytester):
