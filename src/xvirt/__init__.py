fdb5e23f08a9fe9e7a77fc1e0df8fb0f13ca1327
Simone <simone.giacomelli@gmail.com>
2023-07-05 21:36:05 +0100

    conceptualized XVirt

---
+++ src/xvirt/__init__.py
@@ -0,0 +1,20 @@
+class XVirt:
+
+    def remote_path(self) -> str:
+        """Returns the path to be ignored locally and executed remotely."""
+        raise NotImplementedError()
+
+    def run(self):
+        """
+        Should fire the remote execution of the tests and setup the
+        infrastructure to receive the events from the remote.
+        """
+        pass
+
+    def read_event(self) -> str:
+        """Returns the next event received from the remote pytest."""
+        pass
+
+    def finalize(self):
+        """Finalize the remote execution."""
+        pass
