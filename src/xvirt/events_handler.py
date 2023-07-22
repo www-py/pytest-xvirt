d84bab0b87f0fa2d3d2326a1716e043bf63efd6a
Simone <simone.giacomelli@gmail.com>
2023-07-22 18:19:17 +0100

    event order is ensured even if the arrival order is scrambled

--- src/xvirt/events_handler.py
+++ src/xvirt/events_handler.py
@@ -2,15 +2,66 @@ from xvirt import XVirt
 from xvirt.events import EvtCollectionFinish, EvtRuntestLogreport, Evt
 
 
+def _order_events2(xvirt_instance: XVirt):
+    def re():
+        event = xvirt_instance.recv_event()
+        if event is None:
+            return None
+        return Evt.from_json(event)
+
+    events = []
+    while True:
+        evt = re()
+        if evt is None:
+            yield None
+            return
+
+        if isinstance(evt, EvtCollectionFinish):
+            yield evt
+            break
+        else:
+            events.append(evt)
+
+    for evt in events:
+        yield evt
+
+    while True:
+        yield re()
+
+
+def _order_events(xvirt_instance: XVirt):
+    typed_events = []
+    collection_finished_received = False
+
+    def read_event():
+        nonlocal collection_finished_received
+        if collection_finished_received:
+            if len(typed_events) == 0:
+                return xvirt_instance.recv_event()
+            else:
+                return typed_events.pop(0)
+        else:
+            event = xvirt_instance.recv_event()
+            if event is None:
+                return None
+            while True:
+                evt = Evt.from_json(event)
+                if isinstance(evt, EvtCollectionFinish):
+                    collection_finished_received = True
+                typed_events.append(evt)
+                return event
+
+    return read_event
+
+
 def make(file_path, parent):
     def events_handler(xvirt_instance: XVirt):
-        recv_event = xvirt_instance.recv_event
-        collection_finish_json = recv_event()
-        if collection_finish_json is None:  # this means that the user did not implement the remote side
-            return None
-        evt_cf = Evt.from_json(collection_finish_json)
 
-        assert isinstance(evt_cf, EvtCollectionFinish)
+        recv_event = _order_events2(xvirt_instance)
+        evt_cf = next(recv_event)
+        if evt_cf is None:  # this means that the user did not implement the remote side
+            return None
+
         from xvirt.collectors import VirtCollector
         result = VirtCollector.from_parent(parent, name=file_path.name)
         result.nodeid_array = evt_cf.node_ids
@@ -19,7 +70,7 @@ def make(file_path, parent):
         config = parent.config
         recv_count = 0
         while recv_count < len(evt_cf.node_ids):
-            evt_rep = Evt.from_json(recv_event())
+            evt_rep = next(recv_event)
             assert isinstance(evt_rep, EvtRuntestLogreport)
             rep = config.hook.pytest_report_from_serializable(config=config, data=evt_rep.data)
             config.hook.pytest_runtest_logreport(report=rep)
